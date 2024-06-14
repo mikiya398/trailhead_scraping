0a3f14e7be007250abb9e5c1491163f52e46d3a3
mikiyakitamura <mikiyalovejosuke@outlook.jp>
2024-06-14 19:02:31 +0900

    commit test1

--- src/trailblazerProfile_scraping_prop.py
+++ src/trailblazerProfile_scraping_prop.py
@@ -2,5 +2,3 @@ user_data_dir = "/Users/mikiyakitamura/Library/Application Support/Google/Chrome
 profile_directory = "Profile 1"
 input_trailblazerId_file = "./data/input/trailblazerIds.csv"
 input_trailhead_file = './data/input/extract.csv'
-faefewjfr=''
-da=''
